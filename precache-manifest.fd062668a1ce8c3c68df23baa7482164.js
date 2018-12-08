d4d52656de37170e6121182651d8d6b98470702b
demikhovr <demikhovr@gmail.com>
2018-12-08 17:12:38 +0300

    Updates

--- precache-manifest.fd062668a1ce8c3c68df23baa7482164.js
+++ precache-manifest.fd062668a1ce8c3c68df23baa7482164.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/insideland/static/js/runtime~main.199d1f1e.js"
   },
   {
-    "revision": "c0902d1b11caedac8d38",
-    "url": "/insideland/static/js/main.c0902d1b.chunk.js"
+    "revision": "de0e9d0be2ae3327c088",
+    "url": "/insideland/static/js/main.de0e9d0b.chunk.js"
   },
   {
     "revision": "6307ebc41041c37a10d2",
     "url": "/insideland/static/js/1.6307ebc4.chunk.js"
   },
   {
-    "revision": "c0902d1b11caedac8d38",
+    "revision": "de0e9d0be2ae3327c088",
     "url": "/insideland/static/css/main.6c271d11.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/insideland/static/css/1.5551bf46.chunk.css"
   },
   {
-    "revision": "d39930bb4513e6e3d4c082878674e78d",
+    "revision": "876db0b3ff8f8bce1c0bc116e2fd3c72",
     "url": "/insideland/index.html"
   }
 ];
