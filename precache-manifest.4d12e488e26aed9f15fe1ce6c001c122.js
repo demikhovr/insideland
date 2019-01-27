1fba9cfdd62acad4dce172f7b4a4509e94192953
demikhovr <demikhovr@gmail.com>
2019-01-27 21:01:07 +0300

    Updates

---
+++ precache-manifest.4d12e488e26aed9f15fe1ce6c001c122.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "199d1f1ec350875aa7de",
+    "url": "/insideland/static/js/runtime~main.199d1f1e.js"
+  },
+  {
+    "revision": "ca76f675ecc56f302121",
+    "url": "/insideland/static/js/main.ca76f675.chunk.js"
+  },
+  {
+    "revision": "38a09e8157d6687d81df",
+    "url": "/insideland/static/js/1.38a09e81.chunk.js"
+  },
+  {
+    "revision": "ca76f675ecc56f302121",
+    "url": "/insideland/static/css/main.afb336f1.chunk.css"
+  },
+  {
+    "revision": "38a09e8157d6687d81df",
+    "url": "/insideland/static/css/1.0a9b5e57.chunk.css"
+  },
+  {
+    "revision": "581150ec3dacdad515bc7579299a5f84",
+    "url": "/insideland/index.html"
+  }
+];
