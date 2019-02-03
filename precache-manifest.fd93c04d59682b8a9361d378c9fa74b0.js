fc3fbb4c42f9c902111179278be36d390f6b9b03
demikhovr <demikhovr@gmail.com>
2019-02-03 20:38:37 +0300

    Updates

--- precache-manifest.fd93c04d59682b8a9361d378c9fa74b0.js
+++ precache-manifest.fd93c04d59682b8a9361d378c9fa74b0.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/insideland/static/js/runtime~main.199d1f1e.js"
   },
   {
-    "revision": "ee4c02e7ec1c1b79f884",
-    "url": "/insideland/static/js/main.ee4c02e7.chunk.js"
+    "revision": "91347c96066621347864",
+    "url": "/insideland/static/js/main.91347c96.chunk.js"
   },
   {
     "revision": "a851e5239aa84a930d93",
     "url": "/insideland/static/js/1.a851e523.chunk.js"
   },
   {
-    "revision": "ee4c02e7ec1c1b79f884",
+    "revision": "91347c96066621347864",
     "url": "/insideland/static/css/main.395b3a56.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/insideland/static/css/1.cff32a3d.chunk.css"
   },
   {
-    "revision": "013a3a5e1b0452af9a970756c1ab0016",
+    "revision": "cc2e3214453af7d4cfa4d5d50f7dca6e",
     "url": "/insideland/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/insideland/precache-manifest.480205dc37f973c8da720b66c3eff6d4.js"
+  "/insideland/precache-manifest.fd93c04d59682b8a9361d378c9fa74b0.js"
 );
 
 workbox.clientsClaim();
