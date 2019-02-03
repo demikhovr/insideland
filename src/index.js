77dda2c26cd30a23e30a761a1744872a9486ff2b
demikhovr <demikhovr@gmail.com>
2019-02-03 08:39:20 +0300

    add redux, react-redux, redux-thunk, add store to index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import 'normalize.css';
 import './index.css';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
+import rootReducer from './store/reducers/rootReducer';
+
+const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(thunk),
+  ),
+);
 
 const app = (
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.querySelector('.root'));
